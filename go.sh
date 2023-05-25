b9c52be5692c4dc9f0f6713d2baff09f3598fc29
MD Hasan Raja <127471122+HasanHR@users.noreply.github.com>
2023-05-25 16:40:00 -0400

    Update go.sh

--- go.sh
+++ go.sh
@@ -11,6 +11,21 @@ sudo tar -C /usr/local -xzf "go$VERSION.linux-amd64.tar.gz"
 # Set up environment variables
 echo "Setting up environment variables"
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $GITHUB_ENV
+
+# printing installed go version
+go version
 }
 
+
+swag_install () {
+go install github.com/swaggo/swag/cmd/swag@latest
+version=$(swag --version 2>&1)
+if [ $? -eq 0 ]; then
+  echo "Swag version: $version"
+else
+  echo "Swag is not installed or an error occurred."
+}
+
+
 go_install
+swag_install
